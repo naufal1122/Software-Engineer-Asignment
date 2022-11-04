326bba6cdaa5975e2c3135a1a1810d9aa207840b
dewantoro_naufal007 <dewantoronaufal@student.uns.ac.id>
2022-11-04 16:56:02 +0700

    commit

---
+++ app/src/main/java/com/d3ti/software_engineer_asignment/one.java
@@ -0,0 +1,14 @@
+package com.d3ti.software_engineer_asignment;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class one extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_one);
+    }
+}
---
+++ app/src/main/java/com/d3ti/software_engineer_asignment/three.java
@@ -0,0 +1,14 @@
+package com.d3ti.software_engineer_asignment;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class three extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_three);
+    }
+}
